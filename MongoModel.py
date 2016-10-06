a932078cf15df9f9677c3891e39ecc56c3a90968
Mauricio Amezcua <mamezcua94@gmail.com>
2016-10-06 14:51:32 -0500

    speed up

--- MongoModel.py
+++ MongoModel.py
@@ -3,30 +3,33 @@ class Model:
 	def __init__(self, db, tName):
 		self.db = db
 		self.tName = tName
+		self.exists = self.tName in self.db.collection_names()
 
 	def createModel(self) :
-		if self.tName in self.db.collection_names():
+		if self.exists:
 			print ("model already exists")
 		else:
 			print("Model " + self.tName + " created.")
 			self.db.create_collection(self.tName)
+			self.exists = True
 
 	def deleteModel(self):
-		if self.tName in self.db.collection_names():
+		if self.exists:
 			print("Model " + self.tName + " deleted.")
 			self.db[self.tName].drop()
+			self.exists = False
 		else:
 			print ("model does not exist")
 
 	def printModel(self):
-		if self.tName in self.db.collection_names():
+		if self.exists:
 			for item in self.db[self.tName].find():
 				print item
 		else:
 			print ("model does not exist")
-			
+
 	def insertIntoModel(self, items):
-		if self.tName in self.db.collection_names():
+		if exists:
 			for item in items:
 				row_id = self.db[self.tName].count()
 				item['row_id'] = row_id
